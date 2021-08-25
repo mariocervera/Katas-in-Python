ea60ca9451fb3400d44be5b24fa2fe6ff8420003
Mario Cervera <mariocervera@gmail.com>
2021-08-25 21:24:08 +0200

    Refactoring RPN Calculator

--- RPNCalculator/RPNCalculator.py
+++ RPNCalculator/RPNCalculator.py
@@ -10,19 +10,19 @@ operators = ['+', '-', '*', '/']
 
 
 def evaluate(rpn_expression):
-    return evaluate_expression_elements(rpn_expression.split())
+    return __evaluate_expression_elements(rpn_expression.split())
 
 
-def evaluate_expression_elements(elements):
+def __evaluate_expression_elements(elements):
     if len(elements) == 1:
         return int(elements[0])
     elif len(elements) == 3:
-        return evaluate_operation_without_nesting(elements)
+        return __evaluate_operation_without_nesting(elements)
     else:
-        return evaluate_operation_with_nesting(elements)
+        return __evaluate_operation_with_nesting(elements)
     
 
-def evaluate_operation_without_nesting(elements):
+def __evaluate_operation_without_nesting(elements):
     operand1 = int(elements[0])
     operand2 = int(elements[1])
     operator = elements[2]
@@ -30,19 +30,24 @@ def evaluate_operation_without_nesting(elements):
     return operations_table[operator](operand1, operand2)
 
 
-def evaluate_operation_with_nesting(elements):
-    operator_position = find_operator_position(elements)
+def __evaluate_operation_with_nesting(elements):
+    prefix, op_without_nesting, suffix = __split_operation(elements)
+    new_operation = prefix + [__evaluate_operation_without_nesting(op_without_nesting)] + suffix
     
-    prefix = elements[0 : operator_position-2]
-    elements_for_operation = elements[operator_position-2 : operator_position+1]
-    suffix = elements[operator_position+1 :]
-    
-    new_operation = prefix + [str(evaluate_operation_without_nesting(elements_for_operation))] + suffix
-    
-    return evaluate_expression_elements(new_operation)
+    return __evaluate_expression_elements(new_operation)
 
 
-def find_operator_position(elements):
+def __split_operation(elements):
+    operator_pos = __find_first_operator_position(elements)
+    
+    prefix = elements[0 : operator_pos-2]
+    operation_without_nesting = elements[operator_pos-2 : operator_pos+1]
+    suffix = elements[operator_pos+1 :]
+
+    return (prefix, operation_without_nesting, suffix)
+
+
+def __find_first_operator_position(elements):
     for i in range(0, len(elements)):
         if elements[i] in operators:
             return i
